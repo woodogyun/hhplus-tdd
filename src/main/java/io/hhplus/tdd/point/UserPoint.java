823f9ebfe4788416bd9a089fe6e63a81cca48963
DoGyunWoo <wo0170@naver.com>
2024-12-19 23:36:42 +0900

    feat: 포인트 음수 체크 추가

--- src/main/java/io/hhplus/tdd/point/UserPoint.java
+++ src/main/java/io/hhplus/tdd/point/UserPoint.java
@@ -1,10 +1,18 @@
 package io.hhplus.tdd.point;
 
+import io.hhplus.tdd.exception.NegativePointException;
+
 public record UserPoint(
         long id,
         long point,
         long updateMillis
 ) {
+
+    public UserPoint {
+        if (point < 0) {
+            throw new NegativePointException("Point cannot be negative: " + point);
+        }
+    }
 
     public static UserPoint empty(long id) {
         return new UserPoint(id, 0, System.currentTimeMillis());
