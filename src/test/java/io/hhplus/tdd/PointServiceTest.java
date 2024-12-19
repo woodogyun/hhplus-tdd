6b5d644593938ba40694597cd54625352b2cfa3c
DoGyunWoo <wo0170@naver.com>
2024-12-19 19:38:57 +0900

    test: 잔고 부족 코드

--- src/test/java/io/hhplus/tdd/PointServiceTest.java
+++ src/test/java/io/hhplus/tdd/PointServiceTest.java
@@ -10,6 +10,7 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.Mockito.doReturn;
 @ExtendWith(MockitoExtension.class)
 public class PointServiceTest {
@@ -27,10 +28,24 @@ public class PointServiceTest {
         long amount = 100L;
         long updateMillis = 0L;
         UserPoint userPoint = new UserPoint(id, amount, updateMillis);
-        doReturn(new UserPoint(id, amount, updateMillis)).when(userPointTable).selectById(id);
+        doReturn(userPoint).when(userPointTable).selectById(id);
         UserPoint returnUserPoint = pointService.selectById(id);
         assertEquals(returnUserPoint.point(), userPoint.point());
         assertEquals(returnUserPoint.id(), userPoint.id());
         assertEquals(returnUserPoint.updateMillis(), userPoint.updateMillis());
+    }
+    
+    @Test
+    @DisplayName("id로 포인트 사용")
+    void 잔고_부족() {
+        //given
+        long id = 1L;
+        long amount = 100L;
+        long millis = 10L;
+        UserPoint userPoint = new UserPoint(id, amount, millis);
+        //when
+        doReturn(userPoint).when(userPointTable).selectById(id);
+        //then
+        assertThrows(RuntimeException.class, () -> pointService.use(id, amount + 1L));
     }
 }
