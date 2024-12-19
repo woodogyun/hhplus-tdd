32a7e952be872ba98a65017aef7255fc2ccdf45f
DoGyunWoo <wo0170@naver.com>
2024-12-20 00:56:41 +0900

    feat: userPoint 검증 분리

--- src/main/java/io/hhplus/tdd/point/UserPoint.java
+++ src/main/java/io/hhplus/tdd/point/UserPoint.java
@@ -1,18 +1,10 @@
 package io.hhplus.tdd.point;
 
-import io.hhplus.tdd.exception.NegativePointException;
-
 public record UserPoint(
         long id,
         long point,
         long updateMillis
 ) {
-
-    public UserPoint {
-        if (point < 0) {
-            throw new NegativePointException("Point cannot be negative: " + point);
-        }
-    }
 
     public static UserPoint empty(long id) {
         return new UserPoint(id, 0, System.currentTimeMillis());
---
+++ src/main/java/io/hhplus/tdd/vaildation/UserPointValidator.java
@@ -0,0 +1,28 @@
+package io.hhplus.tdd.vaildation;
+
+import org.springframework.stereotype.Component;
+import org.springframework.validation.Errors;
+import org.springframework.validation.Validator;
+import io.hhplus.tdd.point.UserPoint;
+
+@Component
+public class UserPointValidator implements Validator{
+    
+    @Override
+    public boolean supports(Class<?> clazz) {
+        return UserPoint.class.equals(clazz);
+    }
+
+    @Override
+    public void validate(Object target, Errors errors) {
+        UserPoint userPoint = (UserPoint) target;
+
+        if (userPoint.point() < 0) {
+            errors.rejectValue("point", "NegativePointException", "Point must not be negative");
+        }
+
+        if (userPoint.id() < 0) {
+            errors.rejectValue("id", "NegativeIdException", "ID must not be negative");
+        }
+    }
+}
