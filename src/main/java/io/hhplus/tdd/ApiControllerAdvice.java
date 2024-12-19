d1d1ae1c2826f7a581d5c3330921beaeaed301cb
DoGyunWoo <wo0170@naver.com>
2024-12-20 01:06:25 +0900

    feat: advice 예외 추가

--- src/main/java/io/hhplus/tdd/ApiControllerAdvice.java
+++ src/main/java/io/hhplus/tdd/ApiControllerAdvice.java
@@ -4,6 +4,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+import io.hhplus.tdd.exception.NegativeIdException;
 import io.hhplus.tdd.exception.NegativePointException;
 
 @RestControllerAdvice
@@ -15,6 +16,11 @@ class ApiControllerAdvice extends ResponseEntityExceptionHandler {
 
     @ExceptionHandler(value = NegativePointException.class)
     public ResponseEntity<ErrorResponse> NegativePointException(Exception e) {
+        return ResponseEntity.status(500).body(new ErrorResponse("500", "에러가 발생했습니다."));
+    }
+
+    @ExceptionHandler(value = NegativeIdException.class)
+    public ResponseEntity<ErrorResponse> NegativeIdException(Exception e) {
         return ResponseEntity.status(500).body(new ErrorResponse("500", "에러가 발생했습니다."));
     }
 }
