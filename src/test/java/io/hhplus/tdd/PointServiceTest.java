754d43b1e6256b8a464116367f6b797432e87cda
DoGyunWoo <wo0170@naver.com>
2024-12-19 22:12:58 +0900

    test: 이력 조회 테스트 작성성

--- src/test/java/io/hhplus/tdd/PointServiceTest.java
+++ src/test/java/io/hhplus/tdd/PointServiceTest.java
@@ -2,7 +2,9 @@ package io.hhplus.tdd;
 import io.hhplus.tdd.database.PointHistoryTable;
 import io.hhplus.tdd.database.UserPointTable;
 import io.hhplus.tdd.exception.NegativePointException;
+import io.hhplus.tdd.point.PointHistory;
 import io.hhplus.tdd.point.PointService;
+import io.hhplus.tdd.point.TransactionType;
 import io.hhplus.tdd.point.UserPoint;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -13,6 +15,7 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.Mockito.doReturn;
+import java.util.List;
 @ExtendWith(MockitoExtension.class)
 public class PointServiceTest {
     @InjectMocks
@@ -55,12 +58,41 @@ public class PointServiceTest {
     void 유저_포인트가_음수인_경우() {
         //given
         long id = 1L;
-        long amount = -100L;
+        long negativePoint = -100L;
         long millis = 10L;
          // then
-        assertThrows(NegativePointException.class, () -> {
-            new UserPoint(id, amount, millis);
-        });
+        assertThrows(NegativePointException.class, () -> new UserPoint(id, negativePoint, millis));
+    }
+
+    @Test
+    @DisplayName("유저 이용 내역 조회")
+    void 유저_이용_내역_조회() {
+        long id = 1L;
+        long userId = 2L;
+        long amount1 = 200L;
+        long amount2 = 100L;
+        long updateMillis1 = 1L;
+        long updateMillis2 = 2L;
+
+        List<PointHistory> userHistories = List.of(
+            new PointHistory(id, userId, amount1, TransactionType.CHARGE, updateMillis1),
+            new PointHistory(id + 1, userId, amount2, TransactionType.USE, updateMillis2));
+
+        doReturn(userHistories).when(pointHistoryTable).selectAllByUserId(userId);
+
+        List<PointHistory> returnPointHistories = pointService.selectAllByUserId(userId);
+        assertEquals(2, returnPointHistories.size());
+        assertEquals(userHistories.get(0).id(), returnPointHistories.get(0).id());
+        assertEquals(userHistories.get(0).userId(), returnPointHistories.get(0).userId());
+        assertEquals(userHistories.get(0).amount(), returnPointHistories.get(0).amount());
+        assertEquals(userHistories.get(0).type(), returnPointHistories.get(0).type());
+        assertEquals(userHistories.get(0).updateMillis(), returnPointHistories.get(0).updateMillis());
+
+        assertEquals(userHistories.get(1).id(), returnPointHistories.get(1).id());
+        assertEquals(userHistories.get(1).userId(), returnPointHistories.get(1).userId());
+        assertEquals(userHistories.get(1).amount(), returnPointHistories.get(1).amount());
+        assertEquals(userHistories.get(1).type(), returnPointHistories.get(1).type());
+        assertEquals(userHistories.get(1).updateMillis(), returnPointHistories.get(1).updateMillis());
     }
     
 }
